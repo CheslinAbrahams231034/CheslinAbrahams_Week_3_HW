ff0e5188f16a91dd8078029f67b1c44656eac982
CheslinAbrahams231034 <160215424+CheslinAbrahams231034@users.noreply.github.com>
2024-05-15 11:04:55 +0200

    work done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log(total);
+
+function tester(){
+    if (bool == true){
+        document.getElementById('display').innerHTML = "Great You on Our Side";
+    }else {
+        document.getElementById('display').innerHTML = "JOIN US IN THE DARK SIDE!";
+    }
+    
+}
